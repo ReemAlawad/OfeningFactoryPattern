a6723dfa8dfeff1888c3ac3091ff9fc2fddc203d
ReemAlawad <alawadr9@gmail.com>
2024-02-01 06:10:38 +0100

    Add project files.

---
+++ OfeningFactoryPattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using FactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace OfeningFactoryPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var  container = new UnityContainer();
+            //container.RegisterType<IRapportGenerator, ExcelRapportGenerator>("excel");
+            //container.RegisterType<IRapportGenerator, PdfRapportGenerator>("pdf");
+            //container.RegisterType<IRapportGenerator, WordRapportGenerator>("word");
+
+            //var excel = container.Resolve<IRapportGenerator>("excel");
+            //var pdf = container.Resolve<IRapportGenerator>("pdf");
+            //var word = container.Resolve<IRapportGenerator>("word");
+            //excel.GenereerRapport();
+            //pdf.GenereerRapport();
+            //word.GenereerRapport();
+
+
+
+
+            RapportGeneratorFactory factory = new RapportGeneratorFactory();
+
+
+            IRapportGenerator pdfGenerator = factory.GetRapportGenerator("PDF");
+            pdfGenerator.GenereerRapport();
+
+            IRapportGenerator excelGenerator = factory.GetRapportGenerator("EXCEL");
+            excelGenerator.GenereerRapport();
+
+            IRapportGenerator wordGenerator = factory.GetRapportGenerator("WORD");
+            wordGenerator.GenereerRapport();
+        }
+      
+    }
+}
---
+++ OfeningFactoryPattern/RapportGeneratorFactory.cs
@@ -0,0 +1,27 @@
+﻿using FactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfeningFactoryPattern
+{
+    internal class RapportGeneratorFactory
+    {
+        public IRapportGenerator GetRapportGenerator(string type)
+        {
+            switch (type.ToUpper())
+            {
+                case "PDF":
+                    return new PdfRapportGenerator();
+                case "EXCEL":
+                    return new ExcelRapportGenerator();
+                case "WORD":
+                    return new WordRapportGenerator();
+                default:
+                    throw new ArgumentException("Ongeldig rapporttype");
+            }
+        }
+    }
+}
